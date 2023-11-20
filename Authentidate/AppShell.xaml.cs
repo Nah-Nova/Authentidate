b4cf3bd81115fcc974d94dcbe2050898b7bb3f09
Nah-Nova <noaheutz@hotmail.nl>
2023-11-20 16:33:47 +0100

    Initial Commit

---
+++ Authentidate/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Authentidate;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Authentidate/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AuthentiDate.Views;
+
+namespace Authentidate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            string enteredUsername = UsernameEntry.Text;
+            string enteredPassword = PasswordEntry.Text;
+
+            // Hardcoded credentials for validation
+            string validUsername = "Noa Heutz";
+            string validPassword = "Hoihoi098!";
+
+            if (enteredUsername == validUsername && enteredPassword == validPassword)
+            {
+                // Successful login, navigate to the next page or perform any desired action
+                DisplayAlert("Login Successful", "Welcome, " + enteredUsername, "OK");
+                // Then navigate to the next page 
+                await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                // Invalid login, display an error message
+                DisplayAlert("Login Failed", "Invalid username or password. Please try again.", "OK");
+            }
+        }
+    }
+}
