2b38a32b392133c48b9219754e456355a562d65b
Nah-Nova <noaheutz@hotmail.nl>
2023-11-28 17:12:33 +0100

    feat: fake tinder swipe list

---
+++ Authentidate/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AuthentiDate.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
+
--- Authentidate/Views/HomePage.xaml.cs
+++ Authentidate/Views/HomePage.xaml.cs
@@ -1,13 +1,36 @@
 using System;
+using System.Collections.ObjectModel;
+using AuthentiDate.Models;
 
 namespace AuthentiDate.Views
 {
-    public partial class HomePage : ContentPage
+    public partial class HomePage : TabbedPage
     {
+        public ObservableCollection<Profile> Profiles { get; set; }
+
         public HomePage()
         {
             InitializeComponent();
             SetWelcomeMessage();
+            InitializeProfiles();
+            BindingContext = this;
+        }
+
+        private void InitializeProfiles()
+        {
+            Profiles = new ObservableCollection<Profile>
+            {
+                 new Profile { Name = "Jordan", ImageUrl = "https://placebeard.it/640x360" },
+                 new Profile { Name = "Chris", ImageUrl = "https://placebeard.it/641x360" },
+                 new Profile { Name = "Pat", ImageUrl = "https://placebeard.it/642x360" },
+                 new Profile { Name = "Taylor", ImageUrl = "https://placebeard.it/643x360" },
+                 new Profile { Name = "Jamie", ImageUrl = "https://placebeard.it/644x360" },
+                 new Profile { Name = "Morgan", ImageUrl = "https://placebeard.it/645x360" },
+                 new Profile { Name = "Casey", ImageUrl = "https://placebeard.it/646x360" },
+                 new Profile { Name = "Riley", ImageUrl = "https://placebeard.it/647x360" },
+                 new Profile { Name = "Jordan", ImageUrl = "https://placebeard.it/648x360" },
+                 new Profile { Name = "Alex", ImageUrl = "https://placebeard.it/649x360" },
+            };
         }
 
         private void SetWelcomeMessage()
@@ -29,6 +52,27 @@ namespace AuthentiDate.Views
             }
 
             WelcomeLabel.Text = $"{welcomeMessage} Welcome to AuthentiDate!";
+        }
+
+        // Event handler for the Dislike button
+        private void OnDislikeClicked(object sender, EventArgs e)
+        {
+            // Implement your logic for Dislike action
+            DisplayAlert("Action", "You disliked a profile.", "OK");
+        }
+
+        // Event handler for the Like button
+        private void OnLikeClicked(object sender, EventArgs e)
+        {
+            // Implement your logic for Like action
+            DisplayAlert("Action", "You liked a profile.", "OK");
+        }
+
+        // Event handler for the Super Like button
+        private void OnSuperLikeClicked(object sender, EventArgs e)
+        {
+            // Implement your logic for Super Like action
+            DisplayAlert("Action", "You super liked a profile.", "OK");
         }
     }
 }
