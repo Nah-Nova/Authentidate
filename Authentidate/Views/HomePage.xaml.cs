75532dd14877aa84ebebc591893624172b003c83
Nah-Nova <noaheutz@hotmail.nl>
2023-12-01 14:12:10 +0100

    feat: tinder swipe v1

--- Authentidate/Views/HomePage.xaml.cs
+++ Authentidate/Views/HomePage.xaml.cs
@@ -1,78 +1,145 @@
-using System;
 using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
 using AuthentiDate.Models;
 
 namespace AuthentiDate.Views
 {
     public partial class HomePage : TabbedPage
     {
-        public ObservableCollection<Profile> Profiles { get; set; }
+        public ObservableCollection<Profile> Profiles { get; private set; }
+        private int _currentProfileIndex = 0;
+
+        private Profile _currentProfile;
+        public Profile CurrentProfile
+        {
+            get => _currentProfile;
+            set
+            {
+                _currentProfile = value;
+                OnPropertyChanged(nameof(CurrentProfile)); // Notify UI of change
+            }
+        }
 
         public HomePage()
         {
             InitializeComponent();
-            SetWelcomeMessage();
             InitializeProfiles();
             BindingContext = this;
+
+            // Validate Profiles collection is not empty before setting initial profile
+            if (Profiles.Any())
+            {
+                CurrentProfile = Profiles[_currentProfileIndex];
+            }
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            SwipeCard.GestureRecognizers.Add(panGesture);
         }
 
         private void InitializeProfiles()
         {
             Profiles = new ObservableCollection<Profile>
             {
-                 new Profile { Name = "Jordan", ImageUrl = "https://placebeard.it/640x360" },
-                 new Profile { Name = "Chris", ImageUrl = "https://placebeard.it/641x360" },
-                 new Profile { Name = "Pat", ImageUrl = "https://placebeard.it/642x360" },
-                 new Profile { Name = "Taylor", ImageUrl = "https://placebeard.it/643x360" },
-                 new Profile { Name = "Jamie", ImageUrl = "https://placebeard.it/644x360" },
-                 new Profile { Name = "Morgan", ImageUrl = "https://placebeard.it/645x360" },
-                 new Profile { Name = "Casey", ImageUrl = "https://placebeard.it/646x360" },
-                 new Profile { Name = "Riley", ImageUrl = "https://placebeard.it/647x360" },
-                 new Profile { Name = "Jordan", ImageUrl = "https://placebeard.it/648x360" },
-                 new Profile { Name = "Alex", ImageUrl = "https://placebeard.it/649x360" },
+                // Add your profiles here
+                new Profile { Name = "Noa Heutz", ImageUrl = "https://streetartcities.com/media/2/2cbe8dc2-7e8b-4743-9576-fff32d79ee11/orig.jpg" },
+                new Profile { Name = "Jaymian-Lee Reinartz", ImageUrl = "https://www.rpwebdesign.nl/wp-content/uploads/2023/10/Thumnbail-1-500x500.png"},
+                new Profile { Name = "Xavier Prickaerts", ImageUrl = "https://www.rpwebdesign.nl/wp-content/uploads/2023/10/IMG_6252-500x500.jpg" },
+                // ... other profiles
             };
         }
 
-        private void SetWelcomeMessage()
+        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
         {
-            DateTime now = DateTime.Now;
-            string welcomeMessage = "";
-
-            if (now.Hour >= 0 && now.Hour < 12)
+            switch (e.StatusType)
             {
-                welcomeMessage = "Good morning!";
+                case GestureStatus.Running:
+                    HandleCardMovement(e.TotalX, e.TotalY);
+                    break;
+                case GestureStatus.Completed:
+                    HandleSwipeDirection(e.TotalX, e.TotalY);
+                    break;
             }
-            else if (now.Hour >= 12 && now.Hour < 18)
+        }
+
+        private void HandleCardMovement(double x, double y)
+        {
+            SwipeCard.TranslationX = x;
+            SwipeCard.TranslationY = y;
+            SwipeCard.Rotation = (x / 1000) * 30; // Adjust rotation angle as needed
+        }
+
+        private void HandleSwipeDirection(double x, double y)
+        {
+            const double swipeThreshold = 20; // Adjust as needed for sensitivity
+            if (Math.Abs(x) > swipeThreshold)
             {
-                welcomeMessage = "Good afternoon!";
+                if (x > 0) // Right Swipe
+                {
+                    SwipeRightAction();
+                }
+                else // Left Swipe
+                {
+                    SwipeLeftAction();
+                }
+                LoadNextProfile();
             }
             else
             {
-                welcomeMessage = "Good evening!";
+                ResetCardPosition();
             }
-
-            WelcomeLabel.Text = $"{welcomeMessage} Welcome to AuthentiDate!";
         }
 
-        // Event handler for the Dislike button
-        private void OnDislikeClicked(object sender, EventArgs e)
+        private void SwipeRightAction()
         {
-            // Implement your logic for Dislike action
-            DisplayAlert("Action", "You disliked a profile.", "OK");
+            DisplayAlert("Match", "You have matched with this profile!", "OK");
         }
 
-        // Event handler for the Like button
-        private void OnLikeClicked(object sender, EventArgs e)
+        private void SwipeLeftAction()
         {
-            // Implement your logic for Like action
-            DisplayAlert("Action", "You liked a profile.", "OK");
+            DisplayAlert("Ignore", "You have ignored this profile.", "OK");
         }
 
-        // Event handler for the Super Like button
-        private void OnSuperLikeClicked(object sender, EventArgs e)
+        private void OnLeftSwipeClicked(object sender, EventArgs e)
         {
-            // Implement your logic for Super Like action
-            DisplayAlert("Action", "You super liked a profile.", "OK");
+            SwipeLeftAction();
+            LoadNextProfile();
+        }
+
+        private void OnRightSwipeClicked(object sender, EventArgs e)
+        {
+            SwipeRightAction();
+            LoadNextProfile();
+        }
+
+        private void OnSkipClicked(object sender, EventArgs e)
+        {
+            // Implement the skip logic here
+            LoadNextProfile();
+        }
+
+        private void LoadNextProfile()
+        {
+            _currentProfileIndex++;
+
+            if (_currentProfileIndex < Profiles.Count)
+            {
+                CurrentProfile = Profiles[_currentProfileIndex];
+                ResetCardPosition();
+            }
+            else
+            {
+                // Optionally, you can reset the profile index to start over
+                 _currentProfileIndex = 0;
+                 CurrentProfile = Profiles[_currentProfileIndex];
+            }
+        }
+
+        private void ResetCardPosition()
+        {
+            SwipeCard.TranslationX = 0;
+            SwipeCard.TranslationY = 0;
+            SwipeCard.Rotation = 0;
         }
     }
 }
